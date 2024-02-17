ab5ede46348ee459ca108e4ac92f04a19a023691
venom <abdo30201080101694@gmail.com>
2024-02-17 18:30:27 +0200

    add images and create container to handle width

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../CSS/header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-container container">
+        <div className="links"></div>
+        <div className="img"></div>
+      </div>
+      <div className="user-img">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
