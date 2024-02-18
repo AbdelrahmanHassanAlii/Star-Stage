3184f5194367d19497903ca362d18fc7e2ff2338
venom <abdo30201080101694@gmail.com>
2024-02-18 08:57:45 +0200

    handle the main idea of the header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Header from "./Components/Header";
+import Content from "./Components/Content";
+import "./Components/App.css";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "../CSS/header.css";
+import sitelogo from "../Images/site-logo.png";
+import userImage from "../Images/user-img.png";
 
 export default function Header() {
   return (
     <div className="header">
       <div className="header-container container">
         <div className="links">
-          <div className="site-logo"><img src="../../Images/site-logo.png" alt="" /></div>
+          <div className="site-logo">
+            <img src={sitelogo} alt="site-logo" />
+          </div>
           <div className="icons">
-            
+            <i class="fa-brands fa-microsoft"></i>
+            <i class="fa-solid fa-film"></i>
+            <i class="fa-solid fa-tv"></i>
+            <i class="fa-solid fa-bookmark"></i>
           </div>
         </div>
         <div className="user-img">
-          <img src="../../Images/user-img.png" alt="" />
+          <img src={userImage} alt="" />
         </div>
       </div>
     </div>
