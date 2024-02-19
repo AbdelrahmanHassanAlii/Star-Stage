0881c5414740caeab3bf14f360cc92a22b2af3c8
venom <abdo30201080101694@gmail.com>
2024-02-19 23:01:52 +0200

    remove which we didn't use

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Header from "./Components/Header";
 import Content from "./Components/Content";
 import "./App.css";
 import Search from "./Components/Search";
-import "./CSS/swiper-bundle-min.css";
-import "./JS/swiper-bundle.min";
-import "./JS/swiper";
 function App() {
   return (
     <div className="App">
--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.css";
 import "swiper/css";
 import "swiper/css/scrollbar";
 import "swiper/css/navigation";
