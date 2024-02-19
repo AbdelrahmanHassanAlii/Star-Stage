00afb8d7dfda44f71f54e3db09a664b1547e3f5b
venom <abdo30201080101694@gmail.com>
2024-02-19 22:37:17 +0200

    finish swiper

--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -2,7 +2,19 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "../CSS/trending.css";
+
+import {
+  Keyboard,
+  Scrollbar,
+  Navigation,
+  Pagination,
+  Autoplay,
+} from "swiper/modules";
 
 export default function Trending() {
   const [trending, setTrending] = useState(null);
@@ -32,43 +44,59 @@ export default function Trending() {
     <div className="trending">
       <h1>Trending</h1>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={1.25}
-        loop={true}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-          },
-          1024: {
-            slidesPerView: 3,
-          },
-        }}
+        slidesPerView={1.05}
+        centeredSlides={false}
+        slidesPerGroupSkip={1}
+        grabCursor={true}
         autoplay={{
-          delay: 5000,
+          delay: 2000,
           disableOnInteraction: false,
         }}
-        navigation={true}
-        pagination={{ clickable: true }}
+        loop={true}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          520: {
+            slidesPerView: 2.1,
+          },
+          1024: {
+            slidesPerView: 4.1,
+          },
+        }}
+        modules={[Keyboard, Scrollbar, Navigation, Pagination, Autoplay]}
+        className="mySwiper"
       >
-        {trending &&
-          trending.map((element) => (
-            <SwiperSlide key={element.id}>
-              <div className="card">
-                <div className="card-image">
-                  <img
-                    src={`${imagePath}${element.backdrop_path}`}
-                    alt=""
-                    className="img"
-                  />
+        <div
+          className="cards"
+          style={{
+            padding: "200px",
+          }}
+        >
+          {trending &&
+            trending.map((element) => (
+              <SwiperSlide key={element.id}>
+                <div className="card">
+                  <div className="card-image">
+                    <img
+                      src={`${imagePath}${element.backdrop_path}`}
+                      alt=""
+                      className="img"
+                    />
+                  </div>
+                  <div className="card-content">
+                    <p className="name">{element.original_title}</p>
+                    <p className="type">{element.media_type}</p>
+                    <p className="year">{element.release_date}</p>
+                  </div>
                 </div>
-                <div className="card-content">
-                  <p className="name">{element.original_title}</p>
-                  <p className="type">{element.media_type}</p>
-                  <p className="year">{element.release_date}</p>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
+              </SwiperSlide>
+            ))}
+        </div>
       </Swiper>
     </div>
   );
