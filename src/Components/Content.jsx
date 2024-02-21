e23832979c5a09fde8893f69df53cfbbb0c560f5
venom <abdo30201080101694@gmail.com>
2024-02-21 22:29:31 +0200

    add top rated movies component

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Trending from "./Trending";
+import TopRatedMovies from "./TopRatedMovies";
 
 export default function Content() {
   return (
     <div className="container">
       <Trending />
+      <TopRatedMovies />
     </div>
   );
 }
