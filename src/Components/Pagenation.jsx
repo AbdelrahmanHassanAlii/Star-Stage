17c3a7c0a844b25d345ade3bf29d966879e00d45
venom <abdo30201080101694@gmail.com>
2024-02-26 23:27:31 +0200

    finish style of cards of recommend section

--- src/Components/Pagenation.jsx
+++ src/Components/Pagenation.jsx
@@ -1,29 +1,3 @@
-// import React from "react";
-// import ReactPaginate from 'react-paginate';
-// import '../CSS/pagination.css'
-
-// export default function Pagenation({showNext, showPrevious}) {
-
-//   const handlePageClick = () => {
-
-//   }
-
-//   const pageCount = 500;
-//   return (
-//     <>
-//       <ReactPaginate
-//         breakLabel="..."
-//         nextLabel="next >"
-//         marginPagesDisplayed={2}
-//         onPageChange={handlePageClick}
-//         pageRangeDisplayed={2}
-//         pageCount={pageCount}
-//         previousLabel="< previous"
-//         containerClassName="mypagination"
-//       />
-//     </>
-//   );
-// }
 import React from "react";
 import ReactPaginate from "react-paginate";
 import "../CSS/pagination.css";
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 export const myKey = `b5bc4bad4378b1f319250c39c5fd689a`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
