f1b5f2ed128a6e3b734c0dc5d4eb1696d897619f
venom <abdo30201080101694@gmail.com>
2024-02-26 10:12:14 +0200

    add pagenation files

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Trending from "./Trending";
 import Recommended from "./Recommended";
+import Pagenation from "./Pagenation";
 
 export default function Content() {
   return (
     <div className="container">
       <Trending />
       <Recommended />
+      <Pagenation />
     </div>
   );
 }
