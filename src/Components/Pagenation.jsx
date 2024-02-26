3605dd9566aa5fc9dce4d673ffde2cd9b72aabe7
unknown <abdo30201080101694@gmail.com>
2024-02-26 15:37:30 +0200

    create the main css file to pagenation

--- src/Components/Pagenation.jsx
+++ src/Components/Pagenation.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import Pagination from "react-bootstrap/Pagination";
+import ReactPaginate from 'react-paginate';
+import '../CSS/pagination.css'
 
 export default function Pagenation() {
+
+  const handlePageClick = () => {
+
+  }
+
+  const pageCount = 500;
   return (
-    <Pagination>
-      <Pagination.First />
-      <Pagination.Prev />
-      <Pagination.Item>{1}</Pagination.Item>
-      <Pagination.Ellipsis />
-
-      <Pagination.Item>{10}</Pagination.Item>
-      <Pagination.Item>{11}</Pagination.Item>
-      <Pagination.Item active>{12}</Pagination.Item>
-      <Pagination.Item>{13}</Pagination.Item>
-      <Pagination.Item disabled>{14}</Pagination.Item>
-
-      <Pagination.Ellipsis />
-      <Pagination.Item>{20}</Pagination.Item>
-      <Pagination.Next />
-      <Pagination.Last />
-    </Pagination>
+    <>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        marginPagesDisplayed={2}
+        // pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        // renderOnZeroPageCount={null}
+        containerClassName="mypagination"
+      />
+    </>
   );
 }
