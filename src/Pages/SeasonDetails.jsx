6a1c2ecd86dcadfa1024cd0c773c6df5b043983e
venom <abdo30201080101694@gmail.com>
2024-04-13 15:40:27 +0200

    refactor function to get series poster path

--- src/Pages/SeasonDetails.jsx
+++ src/Pages/SeasonDetails.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { getSeasonById } from "../JS/seriesFunction";
+import { getSeasonById, getSeriesById, getSeriesPoster } from "../JS/seriesFunction";
 import { useParams } from "react-router-dom";
 import { getYear } from "../JS/globalFunctions";
 
 export default function SeasonDetails() {
-  const { id, number } = useParams();
-  const { posterImage, setPoaterImage } = useState(null);
+  const { id, number, name } = useParams();
+  const [posterImage, setPosterImage] = useState(null); // Fix typo in state name
   const [details, setDetails] = useState(null);
   const imagePath = "https://image.tmdb.org/t/p/w500";
 
@@ -20,6 +20,16 @@ export default function SeasonDetails() {
       });
   }, [id, number]);
 
+  useEffect(() => {
+    getSeriesById(id, name)
+      .then((data) => {
+        setPosterImage(data.backdrop_path); // Set poster image directly
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [id]); // Fetch poster only when ID changes
+
   return (
     <div className="show-details">
       {details ? (
@@ -27,23 +37,22 @@ export default function SeasonDetails() {
           <div
             className="images"
             style={{
-              backgroundImage:
-                details && details.poster_path
-                  ? `url(${imagePath + details.poster_path})`
-                  : "",
+              backgroundImage: posterImage
+                ? `url(${imagePath + posterImage})`
+                : "", // Use posterImage directly
             }}
           >
             <div className="details-overlay"></div>
             <img
-              src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
+              src={`${imagePath + details.poster_path}`}
               alt={details.name}
             />
           </div>
           <div className="details-text">
             <p className="title">
-              {details.name || details.original_title}{" "}
+              {details.name || details.original_name}{" "}
               <span>
-                {getYear(details.release_date) || getYear(details.air_date)}
+                {getYear(details.air_date) || getYear(details.release_date)}
               </span>
             </p>
             <p>{details.overview}</p>
