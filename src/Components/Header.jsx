e66d674dabcf4509367408ed5a419f752f0da2bd
venom <abdo30201080101694@gmail.com>
2024-06-20 17:04:06 +0300

    handle the active link in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../CSS/header.css";
 import sitelogo from "../Images/site-logo.png";
 import userImage from "../Images/user-img.png";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -12,14 +13,22 @@ export default function Header() {
             <img src={sitelogo} alt="site-logo" />
           </div>
           <div className="icons">
-            <i className="fa-brands fa-microsoft"></i>
-            <i className="fa-solid fa-film"></i>
-            <i className="fa-solid fa-tv"></i>
-            <i className="fa-solid fa-bookmark"></i>
+            <NavLink to="/" exact activeClassName="active-link">
+              <i className="fa-brands fa-microsoft"></i>
+            </NavLink>
+            <NavLink to="/films" activeClassName="active-link">
+              <i className="fa-solid fa-film"></i>
+            </NavLink>
+            <NavLink to="/tv" activeClassName="active-link">
+              <i className="fa-solid fa-tv"></i>
+            </NavLink>
+            <NavLink to="/bookmarks" activeClassName="active-link">
+              <i className="fa-solid fa-bookmark"></i>
+            </NavLink>
           </div>
         </div>
         <div className="user-img">
-          <img src={userImage} alt="" />
+          <img src={userImage} alt="User" />
         </div>
       </div>
     </div>
